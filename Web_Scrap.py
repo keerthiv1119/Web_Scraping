697e72e2f24cb30169f3c70f2994869286c5d41d
keerthiv1119 <44571328+keerthiv1119@users.noreply.github.com>
2019-09-24 16:23:05 +0530

    Add files via upload

---
+++ Web_Scrap.py
@@ -0,0 +1,17 @@
+'''
+Author : Keerthi
+Id : N151119
+'''
+import requests
+file = open("users.txt","r+")#reading data from files
+for line in file:
+	k = line.split(':')[0]
+	with open(k+'.jpg', 'wb') as handle:
+		response = requests.get("http://intranet.rguktn.ac.in/SMS/usrphotos/user/"+str(k)+".jpg", stream=True)
+		if not response.ok:
+			print(response)
+		for block in response.iter_content(1024):
+			if not block:
+				break
+			handle.write(block)
+file.close()
